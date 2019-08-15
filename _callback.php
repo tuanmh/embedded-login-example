9f909c1450ebc2e72119b9e9036349247a91888c
Tuan Huynh <hmtuan21@gmail.com>
2019-08-15 10:57:04 +1000

    Add local storage

--- _callback.php
+++ _callback.php
@@ -1,11 +1,15 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="salesforce-community" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
+  <meta name="salesforce-community" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
 	<meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>">
+  <script type="text/javascript">
+    window.localStorage.setItem('id_token', 'abcdefdasds');
+  </script>
 	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
-</head> 
-<body></body>    
+</head>
+<body></body>
 </html>
